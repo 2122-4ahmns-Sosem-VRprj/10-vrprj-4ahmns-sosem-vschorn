9a78302aa09e559e8318a70e06b410b5197535f0
VictoriaSchorn <victoria.schorn03@htl-salzburg.ac.at>
2022-05-13 10:46:50 +0200

    Additions for Minigame1

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,7 +6,7 @@ public class Respawn : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
-
+    
 
     void OnTriggerEnter(Collider other)
     {
